546bcadeb5167b2c60903531298bf44c63911594
isaac-svg <isaacsakyi009@gmail.com>
2024-01-22 17:56:46 -0500

    all

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,22 @@
+# Connect to a server without typing a password using puppet.
+
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
+}
+
+# Accept Password
+
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
+}
+
